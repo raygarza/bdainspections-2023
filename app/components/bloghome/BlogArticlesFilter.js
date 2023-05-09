d9e8281e1a8c7b70f1fe96d5e717cd7c761d0287
Ray Garza <raydev@protonmail.com>
2023-05-09 11:55:44 -0500

    added revalidate to blogs fetch

--- app/components/bloghome/BlogArticlesFilter.js
+++ app/components/bloghome/BlogArticlesFilter.js
@@ -168,14 +168,14 @@ import { API_URL } from "@/config";
 export default async function BlogArticlesFilter(){
   
 
-   const res = await fetch(`${API_URL}/api/articles?populate=*`);
+   const res = await fetch(`${API_URL}/api/articles?populate=*`, { next: { revalidate: 10 }});
    const articles = await res.json();
 
-   const data = await fetch(`${API_URL}/api/writers?populate=*`);
+   const data = await fetch(`${API_URL}/api/writers?populate=*`, { next: { revalidate: 10 }});
    const writers = await data.json();
    const writerArray = writers.data
   
-   const cats = await fetch(`${API_URL}/api/categories`);
+   const cats = await fetch(`${API_URL}/api/categories`, { next: { revalidate: 10 }});
    const categories = await cats.json();
 
  
