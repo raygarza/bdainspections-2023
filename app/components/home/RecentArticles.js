ef2778b6bfcfbdb0ec133c972665f542e1a738e7
Ray Garza <raydev@protonmail.com>
2023-07-13 16:10:25 -0500

    testing article filtering on front page(26)

--- app/components/home/RecentArticles.js
+++ app/components/home/RecentArticles.js
@@ -19,7 +19,7 @@ export default async function RecentArticles() {
   // let length = articlesArray.data.length;
 
 
-  const articles = articlesArray.data.slice(-4,-1);
+  const articles = articlesArray.data.slice(-4,-1).reverse();
 
 
   return (
