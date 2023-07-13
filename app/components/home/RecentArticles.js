4c149728c97df72af52161bff91d6437badd8d8c
Ray Garza <raydev@protonmail.com>
2023-07-13 14:42:40 -0500

    testing article filtering on front page

--- app/components/home/RecentArticles.js
+++ app/components/home/RecentArticles.js
@@ -15,7 +15,7 @@ async function getArticles() {
 export default async function RecentArticles() {
   const articlesData = getArticles();
   const [articlesArray] = await Promise.all([articlesData]);
-  const articles = articlesArray.data.slice(0, 3);
+  const articles = articlesArray.data.reverse().slice(0, 3);
 
 
   return (
