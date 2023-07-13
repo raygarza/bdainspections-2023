fd506aca4c27799835cd6bc63687bbec1dc44b01
Ray Garza <raydev@protonmail.com>
2023-07-13 15:11:51 -0500

    testing article filtering on front page(12)

--- app/components/home/RecentArticles.js
+++ app/components/home/RecentArticles.js
@@ -15,7 +15,7 @@ async function getArticles() {
 export default async function RecentArticles() {
   const articlesData = getArticles();
   const [articlesArray] = await Promise.all([articlesData]);
-  const articles = articlesArray.data.slice(-4, -1);
+  const articles = articlesArray.data.reversed();
 
 
   return (
@@ -30,7 +30,7 @@ export default async function RecentArticles() {
           <div className="py-1 bg-[#434639] w-20 mt-2 mb-6"></div>
 
           <div className="grid grid-cols-3 gap-x-4 space-y-4 sm:space-y-0 lg:mx-0 lg:max-w-none lg:grid-cols-3 ">
-            {articles.map((article) => {
+            {articles.slice(0,3).map((article) => {
               return (
                 <div
                   key={article.id}
